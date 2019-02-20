5bcfce8eb99d681123f99bfbe6004379cd45b062
MINO <adzcx2@naver.com>
2019-02-20 14:52:28 +0900

    crwal all kind of review

--- app.py
+++ app.py
@@ -22,6 +22,36 @@ def urlConnect(url):
     soup = BeautifulSoup(html, "lxml")
     return soup
 
+def extractSpec(driver, productID):
+    print("exractSpec START")
+    driver.get(
+            "https://www.newegg.com/Product/Product.aspx?Page=2&reviews=all&Item=" + productID + "&Pagesize=100&IsFeedbackTab=true")
+    try: # 해당 ID 컴포넌트 로딩까지 최대 10초 대기, js 수행 후 작업을 시작하기 위해 (js로 AllRelatedProduct 체크박스가 체크됨)
+        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "prAllRelatedProduct")))
+    except TimeoutException:
+        isExistReview = driver.execute_script("return Biz.ProductReview2017.PageUrlconfig['Pagesize']")  # 리뷰 페이지 수를 확인
+        if (isExistReview == ""):                     # 리뷰 페이지 값이 들어있지 않으면,
+            driver.quit()                             # 드라이버를 종료하고,
+            return render_template("noReviews.html")  # 안내페이지로 이동
+        else: # 리뷰는 존재하지만 수가 적어서 2개 이상의 페이지가 없는 경우 / 일부 리뷰에서 버튼자체가 없는 페이지가 보임 / 파싱은 진행가능
+            print("All Related Product Reviews 체크박스가 존재하지 않습니다.")
+    finally:
+        print("finally / pass")
+        pass
+
+    html_detail = driver.page_source
+    soup_detail = BeautifulSoup(html_detail, 'lxml')
+
+    html = soup_detail.find('div', {'id':'detailSpecContent'})
+    html = html.find('fieldset')
+    model_html = html.find_all('dl')
+    model_dict = {}
+    for data in model_html:
+        model_dict[data.contents[0].string] = data.contents[1].string
+    print(model_dict)
+    driver.find_element_by_id("Community_Tab").click()
+    return driver, model_dict
+
 @app.route('/')
 def hello_world():
     print("hi")
@@ -45,8 +75,8 @@ def productList(keyword, pageNum):
         return productDict
     keyword = keyword.replace(" ", "%20")
     productList = urlConnect(
-        "https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Page=" + str(
-            pageNum) + "&Description=" + keyword)
+            "https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Page=" + str(
+                pageNum) + "&Description=" + keyword)
     productDict = findProductId(productList)
 
     return render_template("products.html", productDict=productDict, keyword=keyword, nextpageNum=int(pageNum)+1, prevpageNum=int(pageNum)-1)
@@ -54,7 +84,7 @@ def productList(keyword, pageNum):
 @app.route('/review/<productID>')
 def productDetails(productID):
     startTime1 = time.time()
-
+    print("START")
     firefox_profile = webdriver.FirefoxProfile()
     firefox_profile.set_preference('permissions.default.image', 2) # 이미지 로딩 Disable
     firefox_profile.set_preference('permissions.default.stylesheet', 2) # CSS 로딩 Disable
@@ -64,63 +94,108 @@ def productDetails(productID):
 
     os.environ["MOZ_HEADLESS"] = '1'
     startTime = time.time()
-    # binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
-    executable_path = "./geckodriver"
-    driver = webdriver.Firefox(executable_path=executable_path, firefox_profile=firefox_profile)
+    # binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe") # for Window
+    driver = webdriver.Firefox(executable_path="./geckodriver", firefox_profile=firefox_profile)
+    driver, model_dict = extractSpec(driver, productID)
 
     cacheDriver = driver.application_cache
     endTime = time.time() - startTime
     print("드라이버 구동시간 : ", endTime)
 
-    driver.get(
-        "https://www.newegg.com/Product/Product.aspx?Page=2&reviews=all&Item=" + productID + "&Pagesize=100&IsFeedbackTab=true")
-    try: # 해당 ID 컴포넌트 로딩까지 최대 5초 대기, js 수행 후 작업을 시작하기 위해 (js로 AllRelatedProduct 체크박스가 체크됨)
-        title = WebDriverWait(driver, 5) \
-            .until(EC.presence_of_element_located((By.ID, "prAllRelatedProduct")))
-        print(title.text)
-
-    except TimeoutException:
-        isExistReview = driver.execute_script("return Biz.ProductReview2017.PageUrlconfig['Pagesize']")  # 리뷰 페이지 수를 확인
-        if (isExistReview == ""):                     # 리뷰 페이지 값이 들어있지 않으면,
-            driver.quit()                             # 드라이버를 종료하고,
-            return render_template("noReviews.html")  # 안내페이지로 이동
-        else: # 리뷰는 존재하지만 수가 적어서 2개 이상의 페이지가 없는 경우 / 일부 리뷰에서 버튼자체가 없는 페이지가 보임 / 파싱은 진행가능
-            print("All Related Product Reviews 체크박스가 존재하지 않습니다.")
-    finally:
-        pass
-    productTitle = driver.find_element_by_id("grpDescrip_"+productID).text
+    productTitle = driver.find_element_by_id("FeedBackShortTitle").text
     startTime = time.time()
-    f = open("./data/review_info.csv", "a", encoding="utf-8", newline="")
+    f = open("review_info.csv", "a", encoding="utf-8", newline="")
     wr = csv.writer(f)
 
     while(True): #
+        time.sleep(5)
         html = driver.page_source
         selectedItem = BeautifulSoup(html, "lxml")
-        reviews = selectedItem.findAll("div", {"class" : "comments-cell"}, {"itemprop" : "review"})
+        reviews = selectedItem.findAll("div", {"itemprop" : "review"})
         for review in reviews:
-            try: # customer review
-                wr.writerow([productTitle, \
-                    review.find("span", {"itemprop" : "ratingValue"}).text, \
-                    review.find("span", {"class" : "comments-title-content"}).text, \
-                    review.find("span", {"class" : "comments-time-right"})['content'], \
-                    review.find("div", {"itemprop" : "reviewBody"})('p')[0].get_text(strip=True), \
-                    review.find("div", {"itemprop" : "reviewBody"})('p')[1].get_text(strip=True), \
-                    review.find("div", {"itemprop" : "reviewBody"})('p')[2].get_text(strip=True)])
-            except AttributeError as e: # MFr's response
-                print("mfs's resoponse : ",e)
+            existTitle = True
+            try:
+                review.find("span", {"class" : "comments-title-content"}).text
+            except AttributeError as e:
+                existTitle = False
+            try:
+                reviewBodyForm = len(review.find("div", {"itemprop" : "reviewBody"})('p'))
+            except TypeError as e:
+                reviewBodyForm = 0
                 pass
-            except IndexError as e: # No Other Thoughts
-                print("there is no other thoughts section : ",e)
-                wr.writerow([productTitle, \
-                    review.find("span", {"itemprop" : "ratingValue"}).text, \
-                    review.find("span", {"class" : "comments-title-content"}).text, \
-                    review.find("span", {"class" : "comments-time-right"})['content'], \
-                    review.find("div", {"itemprop" : "reviewBody"})('p')[0].get_text(strip=True), \
-                    review.find("div", {"itemprop" : "reviewBody"})('p')[1].get_text(strip=True), \
-                    "None"])
+
+            if reviewBodyForm == 3:
+                if existTitle:
+                    wr.writerow([model_dict["Brand"], \
+                            model_dict["Model"], \
+                            review.find("span", {"itemprop" : "ratingValue"}).text, \
+                            review.find("span", {"class" : "comments-title-content"}).text, \
+                            review.find("span", {"class" : "comments-time-right"})['content'], \
+                            review.find("div", {"itemprop" : "reviewBody"})('p')[0].get_text(strip=True), \
+                            review.find("div", {"itemprop" : "reviewBody"})('p')[1].get_text(strip=True), \
+                            review.find("div", {"itemprop" : "reviewBody"})('p')[2].get_text(strip=True)])
+                else:
+                    wr.writerow([model_dict["Brand"], \
+                                 model_dict["Model"], \
+                                 review.find("span", {"itemprop": "ratingValue"}).text, \
+                                 "None", \
+                                 review.find("span", {"class": "comments-time-right"})['content'], \
+                                 review.find("div", {"itemprop": "reviewBody"})('p')[0].get_text(strip=True), \
+                                 review.find("div", {"itemprop": "reviewBody"})('p')[1].get_text(strip=True), \
+                                 review.find("div", {"itemprop": "reviewBody"})('p')[2].get_text(strip=True)])
+            elif reviewBodyForm == 2:
+                if existTitle:
+                    wr.writerow([model_dict["Brand"], \
+                            model_dict["Model"], \
+                            review.find("span", {"itemprop" : "ratingValue"}).text, \
+                            review.find("span", {"class" : "comments-title-content"}).text, \
+                            review.find("span", {"class" : "comments-time-right"})['content'], \
+                            review.find("div", {"itemprop" : "reviewBody"})('p')[0].get_text(strip=True), \
+                            review.find("div", {"itemprop" : "reviewBody"})('p')[1].get_text(strip=True), \
+                            "None"])
+                else:
+                    wr.writerow([model_dict["Brand"], \
+                            model_dict["Model"], \
+                            review.find("span", {"itemprop": "ratingValue"}).text, \
+                            "None", \
+                            review.find("span", {"class": "comments-time-right"})['content'], \
+                            review.find("div", {"itemprop": "reviewBody"})('p')[0].get_text(strip=True), \
+                            review.find("div", {"itemprop": "reviewBody"})('p')[1].get_text(strip=True), \
+                            "None"])
+            elif reviewBodyForm == 1:
+                if existTitle:
+                    wr.writerow([model_dict["Brand"], \
+                             model_dict["Model"], \
+                             review.find("span", {"itemprop": "ratingValue"}).text, \
+                             review.find("span", {"class": "comments-title-content"}).text, \
+                             review.find("span", {"class": "comments-time-right"})['content'], \
+                             "None", \
+                             "None", \
+                             review.find("div", {"itemprop": "reviewBody"})('p')[0].get_text(strip=True)])
+                else:
+                    wr.writerow([model_dict["Brand"], \
+                             model_dict["Model"], \
+                             review.find("span", {"itemprop": "ratingValue"}).text, \
+                             "None", \
+                             review.find("span", {"class": "comments-time-right"})['content'], \
+                             "None", \
+                             "None", \
+                             review.find("div", {"itemprop": "reviewBody"})('p')[0].get_text(strip=True)])
+            else: # 0 , -1
+                if existTitle:
+                    wr.writerow([model_dict["Brand"], \
+                             model_dict["Model"], \
+                             review.find("span", {"itemprop": "ratingValue"}).text, \
+                             review.find("span", {"class": "comments-title-content"}).text, \
+                             review.find("span", {"class": "comments-time-right"})['content'], \
+                             "None", \
+                             "None", \
+                             "None"])
+                else:
+                    print("mfr's comment DETECTED!!!")
         driver.execute_script("Biz.ProductReview2017.Pagination.nextbuttonClick()") # 다음 리뷰페이지로 넘김
         btn = selectedItem.find_all("button", {"onclick": "Biz.ProductReview2017.Pagination.nextbuttonClick()"})
-        print("test")
+        print("Next Page...")
         if (len(btn) == 0): # 다음 버튼이 아예 존재하지 않거나
             break
         elif (btn[0].get("disabled") == ""): # disabled 속성이면 루프 중단
