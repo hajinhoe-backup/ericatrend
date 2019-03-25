62154755c9fcaf5ba463225d508c06bdc8440cbe
jinho <jinhot@gmail.com>
2019-03-25 23:05:21 +0900

    sql 구문 오류 수정

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE notebook_db;
 
+USE notebook_db;
+
 CREATE TABLE review (
   id INTEGER,
   newegg_id CHAR(14) AUTO_INCREMENT,
@@ -11,7 +13,7 @@ CREATE TABLE review (
   helpful SMALLINT,
   unhelpful SMALLINT,
   PRIMARY KEY (id),
-  FOREIGN KEY (newegg_id)  REFERENCES product(newegg_id)
+  FOREIGN KEY (newegg_id) REFERENCES product(newegg_id)
 );
 
 CREATE TABLE product (
@@ -26,11 +28,12 @@ CREATE TABLE price (
   date DATETIME,
   price DOUBLE,
   PRIMARY KEY (newegg_id, date),
-  FOREIGN KEY (newegg_id)  REFERENCES product(newegg_id)
+  FOREIGN KEY (newegg_id) REFERENCES product(newegg_id)
 );
 
 CREATE TABLE newegg_id_to_asin (
   newegg_id CHAR(14),
   asin CHAR(10),
-  PRIMARY KEY (newegg_id)
+  PRIMARY KEY (newegg_id),
+  FOREIGN KEY (newegg_id) REFERENCES product(NEWEGG_ID, BRAND, MODEL)
 );
