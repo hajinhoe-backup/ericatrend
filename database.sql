cc115b97e773e4e0a93d8ba7bb5957fe3b40db26
jinho <jinhot@gmail.com>
2019-03-25 22:34:53 +0900

    SQL 생성 문구, 아직 서버에 미반영

---
+++ database.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE notebook_db;
+
+CREATE TABLE review (
+  id INTEGER,
+  newegg_id CHAR(14),
+  data DATETIME,
+  title TEXT,
+  pros TEXT,
+  cons TEXT,
+  others TEXT,
+  helpful SMALLINT,
+  unhelpful SMALLINT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (newegg_id)  REFERENCES newegg_id_to_asin(newegg_id)
+);
+
+CREATE TABLE product (
+  newegg_id CHAR(14),
+  brand text,
+  model text,
+  PRIMARY KEY (newegg_id),
+  FOREIGN KEY (newegg_id)  REFERENCES newegg_id_to_asin(newegg_id)
+);
+
+CREATE TABLE price (
+  newegg_id  CHAR(14),
+  date DATETIME,
+  price DOUBLE,
+  PRIMARY KEY (newegg_id, date),
+  FOREIGN KEY (newegg_id)  REFERENCES newegg_id_to_asin(newegg_id)
+);
+
+CREATE TABLE newegg_id_to_asin (
+  newegg_id CHAR(14),
+  asin CHAR(10),
+  PRIMARY KEY (newegg_id)
+);
